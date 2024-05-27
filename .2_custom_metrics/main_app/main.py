4ac5fede294bf507726332e8bcaffa0328ce4964
Nikolay Melnikov <mle-user@epdigp5m9elt65vutfog.auto.internal>
2024-05-27 10:35:02 +0000

    export metrics

--- .2_custom_metrics/main_app/main.py
+++ .2_custom_metrics/main_app/main.py
@@ -3,11 +3,10 @@ from fastapi.responses import PlainTextResponse
 from prometheus_fastapi_instrumentator import Instrumentator
 import numpy as np
 from prometheus_client import Histogram
-
-# создание экземпляра FastAPI приложения
+# ваш код здесь — необходимый импорт
+from prometheus_client import Counter
+# создание экземпляра FastAPI-приложения
 app = FastAPI()
-
-np.random.seed(1702)
 
 # инициализируем и запускаем экпортёр метрик
 instrumentator = Instrumentator()
@@ -16,16 +15,25 @@ instrumentator.instrument(app).expose(app)
 main_app_predictions = Histogram(
     # имя метрики
     "main_app_predictions",
-    #описание метрики
+    # описание метрики
     "Histogram of predictions",
-    #указаываем корзины для гистограммы
+    # указываем корзины для гистограммы
     buckets=(1, 2, 4, 5, 10)
 )
 
-# предсказания
+
+# ваш код здесь — объект для сбора метрики
+main_positive_counter = Counter('main_positive_counter', 'Count positive result')
+
+
 @app.get("/predict")
 def predict(x: int, y: int):
-    np.random.seed(int(abs(x)))
+    np.random.seed(x)
     prediction = x+y + np.random.normal(0,1)
     main_app_predictions.observe(prediction)
+    if prediction > 0:
+        main_positive_counter.inc()
+    
+    # ваш код здесь — увеличение метрики счётчика
+    
     return {'prediction': prediction}
